a9e7080376555ab3c99ea1b2bda8f0a0921fa8ae
Vena <strom.vena+gh@gmail.com>
2025-10-02 10:31:20 +0200

    add project for doing "uppgifter"

---
+++ PracticeV41/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+internal class Program
+{
+  private static void Main(string[] args)
+  {
+    Console.WriteLine("2IS048 26071 HT2025");
+
+    Uppgift1();
+  }
+
+  private static void Uppgift1()
+  {
+    Console.WriteLine("=== Uppgift 1 ===");
+  }
+}
