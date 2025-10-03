6b9e89cf494a8d2d8a7826b14b8a82bc442e8cde
Vena <strom.vena+gh@gmail.com>
2025-10-03 10:39:04 +0200

    do uppgift 6

--- PracticeV41/src/uppgifter/Uppgift6.cs
+++ PracticeV41/src/uppgifter/Uppgift6.cs
@@ -5,5 +5,26 @@ internal class Uppgift6
   public static void Run()
   {
     WriteLine("=== Uppgift 6 ===");
+
+    Write("Provide a whole number: ");
+    string response = ReadLine() ?? "";
+
+    if (response == "")
+    {
+      WriteLine("Bad response");
+      return;
+    }
+
+    int parsedInteger = Convert.ToInt32(response);
+
+    // Is odd or even
+    if (parsedInteger % 2 == 0)
+    {
+      WriteLine("Jämnt");
+    }
+    else
+    {
+      WriteLine("Udda");
+    }
   }
 }
