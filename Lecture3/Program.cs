9297c92208091da3fa3b3e2a35ea9ad7bc795208
Vena <strom.vena+gh@gmail.com>
2025-10-04 14:08:31 +0200

    do uppgift 3

--- Lecture3/Program.cs
+++ Lecture3/Program.cs
@@ -7,8 +7,8 @@ internal class Program
     WriteLine("=== 2IS048 26071 HT2025 - Lecture 3 (F3) ===\n");
 
     // Uppgift1.Run();
-    Uppgift2.Run();
-    // Uppgift3.Run();
+    // Uppgift2.Run();
+    Uppgift3.Run();
 
 
     // Wait 200 ms before closing due to JSON-RPC disconnect issues
--- Lecture3/src/uppgifter/Uppgift3.cs
+++ Lecture3/src/uppgifter/Uppgift3.cs
@@ -1,3 +1,4 @@
+using System.Text.RegularExpressions;
 using static System.Console;
 
 internal class Uppgift3
@@ -6,5 +7,36 @@ internal class Uppgift3
   {
     WriteLine("=== Uppgift 3 ===");
 
+    Write("Provide two integers: ");
+    string response = ReadLine() ?? "";
+
+    if (response == "")
+    {
+      WriteLine("Bad input");
+      return;
+    }
+
+    int[] inputSplit = [.. new Regex(@",|\s+").Split(response).Where(s => s != "" && int.TryParse(s, out _)).Select(s => { int.TryParse(s, out int num); return num; })];
+
+    WriteLine("Your inputs: [{0}]", string.Join(", ", inputSplit));
+
+    if (inputSplit.Length != 2)
+    {
+      WriteLine("Only provide 2 numbers");
+      return;
+    }
+
+    if (inputSplit[0] == inputSplit[1])
+    {
+      WriteLine("The integers are equal, {0} = {1}", inputSplit[0], inputSplit[1]);
+    }
+    else if (inputSplit.Any(n => n == 0))
+    {
+      WriteLine("At least one integer is zero");
+    }
+    else
+    {
+      WriteLine("Sum of the provided integers: {0}", inputSplit.Sum());
+    }
   }
 }
