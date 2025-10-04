87184583ed1d4246f52f6992f9197a1af4a57fa7
Vena <strom.vena+gh@gmail.com>
2025-10-04 13:51:37 +0200

    add blank uppgift

--- Lecture3/Program.cs
+++ Lecture3/Program.cs
@@ -5,5 +5,15 @@ internal class Program
   private static void Main(string[] args)
   {
     WriteLine("=== 2IS048 26071 HT2025 - Lecture 3 (F3) ===\n");
+
+    Uppgift1.Run();
+    // Uppgift2.Run();
+    // Uppgift3.Run();
+
+
+    // Wait 200 ms before closing due to JSON-RPC disconnect issues
+    Thread.Sleep(200);
+    WriteLine("\nClosing...");
+    Environment.Exit(0);
   }
 }
---
+++ Lecture3/src/uppgifter/Uppgift2.cs
@@ -0,0 +1,10 @@
+using static System.Console;
+
+internal class Uppgift2
+{
+  public static void Run()
+  {
+    WriteLine("=== Uppgift 2 ===");
+
+  }
+}
