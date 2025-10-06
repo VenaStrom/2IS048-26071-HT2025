5dbb6cb4dbca91314e982c261b281a9d296bbf69
Vena <strom.vena+gh@gmail.com>
2025-10-06 18:25:17 +0200

    do uppgift 2

--- Lecture5/src/uppgifter/Uppgift2.cs
+++ Lecture5/src/uppgifter/Uppgift2.cs
@@ -6,5 +6,11 @@ internal class Uppgift2
   {
     WriteLine("=== Uppgift 2 ===");
 
+    int[] array = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+
+    for (int i = array.Length - 1; i > -1; i--)
+    {
+      WriteLine(array[i]);
+    }
   }
 }
