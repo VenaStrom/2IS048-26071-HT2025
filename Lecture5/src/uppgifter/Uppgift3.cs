6fcd456198cb9b6ad8dd1f62b90af9975b24b6f2
Vena <strom.vena+gh@gmail.com>
2025-10-06 18:32:02 +0200

    do uppgift 3

--- Lecture5/src/uppgifter/Uppgift3.cs
+++ Lecture5/src/uppgifter/Uppgift3.cs
@@ -6,5 +6,20 @@ internal class Uppgift3
   {
     WriteLine("=== Uppgift 3 ===");
 
+    string input = "Skriv ett program som byter ut alla små a:n (gemener) mot stora A:n (versaler) i en string. (a → A)";
+
+    char[] inputSplit = input.ToCharArray();
+
+    for (int i = 0; i < inputSplit.Length; i++)
+    {
+      if (inputSplit[i] == 'a')
+      {
+        inputSplit[i] = 'A';
+      }
+    }
+
+    string recombined = string.Join("", inputSplit);
+
+    WriteLine($"\"{recombined}\"");
   }
 }
