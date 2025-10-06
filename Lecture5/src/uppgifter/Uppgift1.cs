6dc6ddf09466c6d0954e53284de170173621fd21
Vena <strom.vena+gh@gmail.com>
2025-10-06 18:22:08 +0200

    add lecture 5

---
+++ Lecture5/src/uppgifter/Uppgift1.cs
@@ -0,0 +1,10 @@
+using static System.Console;
+
+internal class Uppgift1
+{
+  public static void Run()
+  {
+    WriteLine("=== Uppgift 1 ===");
+
+  }
+}
---
+++ Lecture5/src/uppgifter/Uppgift3.cs
@@ -0,0 +1,10 @@
+using static System.Console;
+
+internal class Uppgift3
+{
+  public static void Run()
+  {
+    WriteLine("=== Uppgift 3 ===");
+
+  }
+}
