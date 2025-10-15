7e2987c7c002a0de72143e4a8bb86a9491afa7f9
Vena <strom.vena+gh@gmail.com>
2025-10-15 11:48:47 +0200

    WIP calculator

--- Calculator/Calculator.cs
+++ Calculator/Calculator.cs
@@ -1,105 +1,150 @@
 ﻿using static System.Console;
+using static StringExtension;
 
 class Calculator
 {
-  static void Main(string[] args)
+  static void Main()
   {
     string line = "";
 
-    Draw(line);
+    ConsoleKey[] allowedKeys = [
+      ConsoleKey.D1,
+      ConsoleKey.D2,
+      ConsoleKey.D3,
+      ConsoleKey.D4,
+      ConsoleKey.D5,
+      ConsoleKey.D6,
+      ConsoleKey.D7,
+      ConsoleKey.D8,
+      ConsoleKey.D9,
+      ConsoleKey.D0,
+      ConsoleKey.Add,
+      ConsoleKey.Subtract,
+      ConsoleKey.Multiply,
+      ConsoleKey.Divide,
+      ConsoleKey.Enter,
+      ConsoleKey.Backspace,
+      ConsoleKey.Spacebar,
+    ];
 
     while (true)
     {
-      string input = ReadKey().Key.ToString();
+      Clear();
+      WriteLine("[CALCULATOR]\n");
+      WriteLine(line + "_");
 
-      if (input == "D1")
+      ConsoleKeyInfo keyInfo = ReadKey();
+
+      if (!allowedKeys.Contains(keyInfo.Key)) continue;
+
+      switch (keyInfo.Key)
       {
-        line += "1";
+        case ConsoleKey.Backspace:
+          if (line.Length == 0) break;
+          WriteLine(keyInfo.Modifiers & ConsoleModifiers.Control);
+          if ((keyInfo.Modifiers & ConsoleModifiers.Control) != 0)
+          {
+            // Remove til next space what control is held
+            int? spaceIndex = line.FindCharIndex(' ');
+            WriteLine(spaceIndex);
+            if (spaceIndex == null) line = "";
+            line = line.Slice(0, spaceIndex);
+          }
+          else
+          {
+            // Remove last character
+            line = line[..(line.Length - 1)];
+          }
+          break;
+
+        default:
+          line += keyInfo.KeyChar;
+          break;
       }
 
-      Draw(line, input);
+      Thread.Sleep(100);
     }
   }
 
-  static void Draw(string line, string pressed = "")
-  {
-    Clear();
-    WriteLine(pressed);
+  // static void Draw(string line, string pressed = "")
+  // {
+  //   Clear();
+  //   WriteLine(pressed);
 
-    string D01 = DrawButton(pressed, "D1", "1");
-    string D02 = DrawButton(pressed, "D2", "2");
-    string D03 = DrawButton(pressed, "D3", "3");
-    string D04 = DrawButton(pressed, "D4", "4");
-    string D05 = DrawButton(pressed, "D5", "5");
-    string D06 = DrawButton(pressed, "D6", "6");
-    string D07 = DrawButton(pressed, "D7", "7");
-    string D08 = DrawButton(pressed, "D8", "8");
-    string D09 = DrawButton(pressed, "D9", "9");
-    string D00 = DrawButton(pressed, "D0", "0");
-    string dot = DrawButton(pressed, "OemPeriod", ".");
-    string sub = DrawButton(pressed, "Subtract", "-");
-    string add = DrawButton(pressed, "Add", "+");
-    string mul = DrawButton(pressed, "Multiply", "×");
-    string div = DrawButton(pressed, "Divide", "÷");
-    string eql = DrawButton(pressed, "Enter", "=");
-    string pi_ = DrawButton(pressed, "P", "π");
-    string del = pressed == "Backspace" ? $"#[<-]" : "# <- ";
+  //   string D01 = DrawButton(pressed, "D1", "1");
+  //   string D02 = DrawButton(pressed, "D2", "2");
+  //   string D03 = DrawButton(pressed, "D3", "3");
+  //   string D04 = DrawButton(pressed, "D4", "4");
+  //   string D05 = DrawButton(pressed, "D5", "5");
+  //   string D06 = DrawButton(pressed, "D6", "6");
+  //   string D07 = DrawButton(pressed, "D7", "7");
+  //   string D08 = DrawButton(pressed, "D8", "8");
+  //   string D09 = DrawButton(pressed, "D9", "9");
+  //   string D00 = DrawButton(pressed, "D0", "0");
+  //   string dot = DrawButton(pressed, "OemPeriod", ".");
+  //   string sub = DrawButton(pressed, "Subtract", "-");
+  //   string add = DrawButton(pressed, "Add", "+");
+  //   string mul = DrawButton(pressed, "Multiply", "×");
+  //   string div = DrawButton(pressed, "Divide", "÷");
+  //   string eql = DrawButton(pressed, "Enter", "=");
+  //   string pi_ = DrawButton(pressed, "P", "π");
+  //   string del = pressed == "Backspace" ? $"#[<-]" : "# <- ";
 
-    int colWidth = 22;
-    string formattedLineWithSet_1 = line.PadRight(colWidth, ' ')[..colWidth];
-    string formattedLineWithSet_2 = "".PadRight(colWidth, ' ');
-    string formattedLineWithSet_3 = "".PadRight(colWidth, ' ');
-    string formattedLineWithSet_4 = "".PadRight(colWidth, ' ');
+  //   int colWidth = 22;
+  //   string formattedLineWithSet_1 = line.PadRight(colWidth, ' ')[..colWidth];
+  //   string formattedLineWithSet_2 = "".PadRight(colWidth, ' ');
+  //   string formattedLineWithSet_3 = "".PadRight(colWidth, ' ');
+  //   string formattedLineWithSet_4 = "".PadRight(colWidth, ' ');
 
-    if (line.Length > colWidth)
-    {
-      formattedLineWithSet_2 = line.PadRight(colWidth * 2, ' ').Substring(colWidth, colWidth);
-    }
-    if (line.Length > colWidth * 2)
-    {
-      formattedLineWithSet_3 = line.PadRight(colWidth * 3, ' ').Substring(colWidth * 2, colWidth);
-    }
-    if (line.Length > colWidth * 3)
-    {
-      formattedLineWithSet_4 = line.PadRight(colWidth * 4, ' ').Substring(colWidth * 3, colWidth);
-    }
+  //   if (line.Length > colWidth)
+  //   {
+  //     formattedLineWithSet_2 = line.PadRight(colWidth * 2, ' ').Substring(colWidth, colWidth);
+  //   }
+  //   if (line.Length > colWidth * 2)
+  //   {
+  //     formattedLineWithSet_3 = line.PadRight(colWidth * 3, ' ').Substring(colWidth * 2, colWidth);
+  //   }
+  //   if (line.Length > colWidth * 3)
+  //   {
+  //     formattedLineWithSet_4 = line.PadRight(colWidth * 4, ' ').Substring(colWidth * 3, colWidth);
+  //   }
 
-    // Lines that wrap get a '<' else ' '
-    formattedLineWithSet_1 += line.Length > colWidth ? " <" : "  ";
-    formattedLineWithSet_2 += line.Length > colWidth * 2 ? " <" : "  ";
-    formattedLineWithSet_3 += line.Length > colWidth * 3 ? " <" : "  ";
-    formattedLineWithSet_4 += line.Length > colWidth * 4 ? " |" : "  ";
-    string stop = line.Length > colWidth * 4 ? "[STOP]" : "      ";
+  //   // Lines that wrap get a '<' else ' '
+  //   formattedLineWithSet_1 += line.Length > colWidth ? " <" : "  ";
+  //   formattedLineWithSet_2 += line.Length > colWidth * 2 ? " <" : "  ";
+  //   formattedLineWithSet_3 += line.Length > colWidth * 3 ? " <" : "  ";
+  //   formattedLineWithSet_4 += line.Length > colWidth * 4 ? " |" : "  ";
+  //   string stop = line.Length > colWidth * 4 ? "[STOP]" : "      ";
 
-    string vis = $"""
-      ▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄
-    #############################
-    ##                         ##
-    ## {formattedLineWithSet_1}##
-    ## {formattedLineWithSet_2}##
-    ## {formattedLineWithSet_3}##
-    ## {formattedLineWithSet_4}##
-    ##         {stop}          ##
-    #############################
-    ##{del}# ( ## ) #▌mod▐{pi_}##
-    #############################
-    ##{D07}{D08}{D09}{div}# √ ###
-    #############################
-    ##{D04}{D05}{D06}{mul}# x²###
-    #############################
-    ##{D01}{D02}{D03}{sub}#   ###
-    ######################{eql}##
-    ##{D00}{dot}# % #{add}#   ###
-    #############################
-    #############################
-      ▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀
-    """.Replace("#", "█");
+  //   string vis = $"""
+  //     ▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄
+  //   #############################
+  //   ##                         ##
+  //   ## {formattedLineWithSet_1}##
+  //   ## {formattedLineWithSet_2}##
+  //   ## {formattedLineWithSet_3}##
+  //   ## {formattedLineWithSet_4}##
+  //   ##         {stop}          ##
+  //   #############################
+  //   ##{del}# ( ## ) #▌mod▐{pi_}##
+  //   #############################
+  //   ##{D07}{D08}{D09}{div}# √ ###
+  //   #############################
+  //   ##{D04}{D05}{D06}{mul}# x²###
+  //   #############################
+  //   ##{D01}{D02}{D03}{sub}#   ###
+  //   ######################{eql}##
+  //   ##{D00}{dot}# % #{add}#   ###
+  //   #############################
+  //   #############################
+  //     ▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀
+  //   """.Replace("#", "█");
 
-    WriteLine(vis);
-  }
+  //   WriteLine(vis);
+  // }
 
-  static string DrawButton(string pressed, string target, string label)
-  {
-    return pressed == target ? $"#[{label}]#" : $"# {label} #";
-  }
+  // static string DrawButton(string pressed, string target, string label)
+  // {
+  //   return pressed == target ? $"#[{label}]#" : $"# {label} #";
+  // }
 }
---
+++ Calculator/src/std.cs
@@ -0,0 +1,33 @@
+
+static class StringExtension
+{
+  public static int? FindCharIndex(this string haystack, char needle)
+  {
+    char[] letters = haystack.ToCharArray();
+
+    for (int i = 0; i < letters.Length; i++)
+    {
+      if (letters[i] == needle) return i;
+    }
+
+    return null;
+  }
+
+  public static string Slice(this string s, int? start = null, int? end = null)
+  {
+    start ??= 0;
+    end ??= s.Length;
+
+    int length = Math.Abs(end.Value - start.Value);
+    char[] letters = new char[length];
+    int letterIndexPointer = 0;
+
+    for (int i = start.Value; i < end.Value; i++)
+    {
+      letters[letterIndexPointer] = s[i];
+      letterIndexPointer++;
+    }
+
+    return new string(letters);
+  }
+}
